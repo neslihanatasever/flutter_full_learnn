4861d468b2a3633d8095095bddfc47e37bf94321
Neslihan Atasever <neslihan_atasvr111@outlook.com>
2022-09-09 17:21:43 +0300

    container_sized_box_learn

---
+++ lib/101/scaffold_learn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldLearnView extends StatelessWidget {
+  const ScaffoldLearnView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_full_learn/101/text_learn.dart';
 import '101/container_sized_box_learn.dart';
+import '101/scaffold_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: ContainerSizedBoxLearn(),
+      home: ScaffoldLearnView(),
     );
   }
 }
