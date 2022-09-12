1e703d30c6c8ce70145b5fd573bdcead04b428bf
Neslihan Atasever <neslihan_atasvr111@outlook.com>
2022-09-12 10:30:54 +0300

    container_sized_box_learn

--- lib/101/scaffold_learn.dart
+++ lib/101/scaffold_learn.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
 
 class ScaffoldLearnView extends StatelessWidget {
   const ScaffoldLearnView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scaffold Samples')),
+      body: const Text('Hello World'),
+      backgroundColor: Colors.brown,
+      extendBody: true,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => Container(
+                  height: 200
+              ));
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      drawer: const Drawer(),
+      bottomNavigationBar: Container(
+        height: 200,
+        decoration:  ProjectContainerDecoration(),
+        child: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.abc_outlined), label: 'A'),
+          BottomNavigationBarItem(icon: Icon(Icons.abc_outlined), label: 'B'),
+        ]),
+      ),
+    );
   }
 }
